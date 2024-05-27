b16f686c0c36b4c5aa715366f30115565d91585b
Ashwinigadad <ashwinigadad3@gmail.com>
2024-05-27 22:19:31 +0530

    added map

--- src/App.js
+++ src/App.js
@@ -24,10 +24,10 @@ class App extends Component {
       <Hello /> */}
       {/* <Count/> */}
 
-      <EventBind/> 
-      <ParentComponent/>
+      {/* <EventBind/> 
+      <ParentComponent/> */}
       {/* <UserGreeting/> */}
-      {/* <ListName/> */}
+      <ListName/>
 
       
 
--- src/components/ListName.js
+++ src/components/ListName.js
@@ -1,12 +1,14 @@
 import React from 'react'
 
 function ListName() {
-    const name=['A','B','C','D']
+    const names=['A','B','C','D']
   return (
     <div>
-        <h1>{name[0]}</h1>
-        <h2>{name[1]}</h2>
-        <h3>{name[2]}</h3>
+      {
+        names.map(name=><h2>{name}</h2>)
+
+      }
+        
       
     </div>
   )
